e8caa84f876a50b10929c42bc03e45bcfd265131
tanka2u <ptanka123@gmail.com>
2024-08-08 22:39:49 +0200

    foodies app dynamic title also

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not Found</h1>
+      <p>Unfortunately, We could not find the requested page or data.</p>
+    </main>
+  );
+}
